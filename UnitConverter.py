5cb04b78f57e5510b0df41c2bcefb123617670d8
seanie5011 <seanie500@hotmail.com>
2022-08-02 19:39:46 +0100

    Formatting dunderscore main
    Added formatting with the def main if name main style

--- UnitConverter.py
+++ UnitConverter.py
@@ -1,14 +1,5 @@
 import PySimpleGUI as sg
 
-# create layout with amount of rows corresponding to number of lists
-# each row contains some elements
-# we can give each a key which is a unique string identifier
-# we can also edit size of each element at will
-layout = [[sg.Input(key='-INPUT-'), sg.Spin(['lbs to kg', 'kg to lbs', 'kg to tonne', 'tonne to kg'], key='-SPIN_UNITS-'), sg.Button('Convert', key='-BUTTON_CONVERT-', size=(5,2))],
-          [sg.Text('OUTPUT', key='-TEXT_OUTPUT-')]]
-
-window = sg.Window('Converter', layout)  # create window with a title and a layout
-
 
 # converter class containing all unit conversions supported
 class Converter:
@@ -28,35 +19,49 @@ class Converter:
         kg = tonne * 1000
         return kg
 
-converter = Converter()
-while True:
-    event, values = window.read()  # returns all events of that frame, along with a dict of all values
 
-    # quit logic
-    if event == sg.WIN_CLOSED:
-        break
+def main():
+    # create layout with amount of rows corresponding to number of lists
+    # each row contains some elements
+    # we can give each a key which is a unique string identifier
+    # we can also edit size of each element at will
+    layout = [[sg.Input(key='-INPUT-'), sg.Spin(['lbs to kg', 'kg to lbs', 'kg to tonne', 'tonne to kg'], key='-SPIN_UNITS-'), sg.Button('Convert', key='-BUTTON_CONVERT-', size=(5, 2))],
+              [sg.Text('OUTPUT', key='-TEXT_OUTPUT-')]]
 
-    # button pressed
-    if event == '-BUTTON_CONVERT-':
-        try:
-            input_value = float(values['-INPUT-'])  # must pass in float as values returns a string
+    window = sg.Window('Converter', layout)  # create window with a title and a layout
 
-            # identify correct conversion
-            if values['-SPIN_UNITS-'] == 'lbs to kg':
-                value = converter.lbs_to_kg(input_value)
-            elif values['-SPIN_UNITS-'] == 'kg to lbs':
-                value = converter.kg_to_lbs(input_value)
-            elif values['-SPIN_UNITS-'] == 'kg to tonne':
-                value = converter.kg_to_tonne(input_value)
-            elif values['-SPIN_UNITS-'] == 'tonne to kg':
-                value = converter.tonne_to_kg(input_value)
-        except ValueError:  # if input value is not float-like
-            print("ValueError: the value inputted is not of correct form type float.")
-            value = "Please enter a float number."
-        except:
-            print("Unknown Error.")
-            value = "ERROR"
+    converter = Converter()
+    while True:
+        event, values = window.read()  # returns all events of that frame, along with a dict of all values
 
-        window['-TEXT_OUTPUT-'].update(value)  # we can update the specified window element with new value
+        # quit logic
+        if event == sg.WIN_CLOSED:
+            break
 
-window.close()
+        # button pressed
+        if event == '-BUTTON_CONVERT-':
+            try:
+                input_value = float(values['-INPUT-'])  # must pass in float as values returns a string
+
+                # identify correct conversion
+                if values['-SPIN_UNITS-'] == 'lbs to kg':
+                    value = converter.lbs_to_kg(input_value)
+                elif values['-SPIN_UNITS-'] == 'kg to lbs':
+                    value = converter.kg_to_lbs(input_value)
+                elif values['-SPIN_UNITS-'] == 'kg to tonne':
+                    value = converter.kg_to_tonne(input_value)
+                elif values['-SPIN_UNITS-'] == 'tonne to kg':
+                    value = converter.tonne_to_kg(input_value)
+            except ValueError:  # if input value is not float-like
+                print("ValueError: the value inputted is not of correct form type float.")
+                value = "Please enter a float number."
+            except:
+                print("Unknown Error.")
+                value = "ERROR"
+
+            window['-TEXT_OUTPUT-'].update(value)  # we can update the specified window element with new value
+
+    window.close()
+
+if __name__ == '__main__':
+    main()
